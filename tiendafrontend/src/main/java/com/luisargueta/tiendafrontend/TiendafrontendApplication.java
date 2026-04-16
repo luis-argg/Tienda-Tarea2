060bebce6048f82d53c966e93ea476a94656159a
Luis Argueta <luissarg102@gmail.com>
2026-04-16 13:50:21 -0600

    Add files via upload

---
+++ tiendafrontend/src/main/java/com/luisargueta/tiendafrontend/TiendafrontendApplication.java
@@ -0,0 +1,13 @@
+package com.luisargueta.tiendafrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendafrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendafrontendApplication.class, args);
+	}
+
+}
